1bc8ba3a51a55f7b5dfc3f83f732a6477138ee44
TOMASZ_REJ <tomaszrej@gmail.com>
2022-08-19 09:31:48 +0200

    version and build number displayed in main screen

--- App.tsx
+++ App.tsx
@@ -7,21 +7,27 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import DeviceInfo from 'react-native-device-info';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.safeArea}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <View style={styles.container}>
-        <Text>HELLO WORLD!</Text>
+        <Text>HELLO WORLD</Text>
+        <Text>VERSION: {DeviceInfo.getVersion()}</Text>
+        <Text>BUILD NUMBER: {DeviceInfo.getBuildNumber()}</Text>
       </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     alignItems: 'center',
